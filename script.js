dcab30fedef77988e85661ce3a070fc7be4163eb
Nishant Godse <125818822+Nishant5397@users.noreply.github.com>
2023-06-29 13:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	get(name)
+	{
+		return this.name;
+	}
+	set(age)
+	{
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log('${this.name} is studying');
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	tech(){
+		console.log('${this.age} is teaching')
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
