847b6c00939d5ecaf7187ea2aee7460723e7541a
Nishant Godse <125818822+Nishant5397@users.noreply.github.com>
2023-06-29 13:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
 class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
 
-  get name() {
-    return this._name;
-  }
+	getName() {
+		return this.name;
+	}
 
-  set name(name) {
-    this._name = name;
-  }
-
-  get age() {
-    return this._age;
-  }
-
-  set age(age) {
-    this._age = age;
-  }
+	setAge(age) {
+		this.age = age;
+	}
 }
 
 class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying.`);
-  }
+	study() {
+		console.log(`${this.getName()} is studying.`);
+	}
 }
 
 class Teacher extends Person {
-  teach() {
-    console.log(`${this.name} is teaching.`);
-  }
+	teach() {
+		console.log(`${this.getName()} is teaching`);
+	}
 }
 
 // Do not change the code below this line
