cd15c6a6486962b0455626223a82d2055936a2a0
Nishant Godse <125818822+Nishant5397@users.noreply.github.com>
2023-08-11 13:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ class Student extends Person {
 
 class Teacher extends Person {
 	teach() {
-		console.log(`${this.getName()} is teaching`);
+		console.log(`${this.getName()} is teaching.`);
 	}
 }
 
